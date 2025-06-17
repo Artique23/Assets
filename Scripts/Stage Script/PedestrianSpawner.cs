3d29e9eb84f73826f69a15112deb326c6faa06f1
Artique23 <kmartinsevilla@gmail.com>
2025-06-17 19:01:37 +0800

    ADDED A BUNCH OF NPC WALKING

--- Scripts/Stage Script/PedestrianSpawner.cs
+++ Scripts/Stage Script/PedestrianSpawner.cs
@@ -12,6 +12,26 @@ public class PedestrianSpawner : MonoBehaviour
         PedestrianWalker walker = npc.GetComponent<PedestrianWalker>();
         walker.SetupWaypoints(this);
     }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        // Draw a sphere at each waypoint (each child)
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform waypoint = transform.GetChild(i);
+            Gizmos.DrawSphere(waypoint.position, 0.3f);
+
+            // Draw a line to the next waypoint (if any)
+            if (i < transform.childCount - 1)
+            {
+                Transform nextWaypoint = transform.GetChild(i + 1);
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(waypoint.position, nextWaypoint.position);
+                Gizmos.color = Color.yellow; // Reset color for spheres
+            }
+        }
+    }
 
     public void Respawn()
     {
